b31ca0ebc41e4f479f1eb175d5dab0fcd15dc1e0
samuele301197 <parisisamu32@gmail.com>
2025-09-04 17:12:44 +0200

    feat: question-1

---
+++ esercizi.js
@@ -0,0 +1,5 @@
+// Code Question 1:
+
+// senza lanciare il codice, prevedo che entrambi i "riferimenti" all'oggetto sono stati modificati, motivo per cui in console apparirà il valore hamburger.name / secondBurger.name = "Double Cheese Burger";
+
+// Oggetto creato in memoria ce n'è uno solo.
