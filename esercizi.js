2444545878e63e1eed54a3f448144bba8a697765
samuele301197 <parisisamu32@gmail.com>
2025-09-04 17:17:46 +0200

    feat: question-2

--- esercizi.js
+++ esercizi.js
@@ -1,5 +1,12 @@
 // Code Question 1:
 
 // senza lanciare il codice, prevedo che entrambi i "riferimenti" all'oggetto sono stati modificati, motivo per cui in console apparirà il valore hamburger.name / secondBurger.name = "Double Cheese Burger";
-
 // Oggetto creato in memoria ce n'è uno solo.
+
+// Code Question 2:
+
+// senza lanciare il codice, prevedo che:
+//  hamburger.ingredients[0] = "Salad",
+//  secondBurger[0] = "Salad".
+// con lo spread operator non si riesce a modificare oggetti annidati.
+//  In memoria ci sono due oggetti.
