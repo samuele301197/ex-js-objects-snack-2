a7bbb54254bd6394beab1a8b546e24e6a40404c0
samuele301197 <parisisamu32@gmail.com>
2025-09-04 17:26:29 +0200

    feat:Question-4

--- esercizi.js
+++ esercizi.js
@@ -1,12 +1,20 @@
 // Code Question 1:
-
 // senza lanciare il codice, prevedo che entrambi i "riferimenti" all'oggetto sono stati modificati, motivo per cui in console apparirà il valore hamburger.name / secondBurger.name = "Double Cheese Burger";
 // Oggetto creato in memoria ce n'è uno solo.
 
 // Code Question 2:
-
 // senza lanciare il codice, prevedo che:
 //  hamburger.ingredients[0] = "Salad",
 //  secondBurger[0] = "Salad".
 // con lo spread operator non si riesce a modificare oggetti annidati.
 //  In memoria ci sono due oggetti.
+
+// Code Question 3:
+// in memoria ci sono 5 oggetti totali:
+// il primo oggetto (hamburger),
+// gli oggetti annidati facenti parte dello stesso (maker, restaurant),
+// le due copie (secondBurger, thirdBurger).
+
+// Code Question 4:
+// Il metodo migliore è lo spread operator, poichè copia anche le funzioni.
+// Il metodo migliore per clonare restaurant è structuredClone perchè copia anche oggetti complessi come Date
